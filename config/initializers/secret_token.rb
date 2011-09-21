013614769539bfd2a58adb07fea6ec3150f9bd7a
xxd <xxdxxd51@hotmail.com>
2011-09-21 16:23:56 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Optimus::Application.config.secret_token = '79dbe4c3bce8dbec6bde369ffc41cfeb6bd5e45c06150f1f2593214baa17f7cfa8e5681ce02e134842fbd4f237826bbd5f659d48806bf87219619cdd3d816b26'
